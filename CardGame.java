3b7327052c9ab129132d075806723a4ea7c7f22a
itsjared501 <jared.lee501@campus.mccd.edu>
2025-01-28 20:41:10 +0000

    add extensions

--- CardGame.java
+++ CardGame.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CardGame {
-
+	
 	private static ArrayList<Card> deckOfCards = new ArrayList<Card>();
 	private static ArrayList<Card> playerCards = new ArrayList<Card>();
 
